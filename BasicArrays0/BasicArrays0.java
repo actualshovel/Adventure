682816fa067f3edfafea5342e4d3c0fa8bb97ab1
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-11-17 14:20:51 -0800

    Add files via upload

--- BasicArrays0/BasicArrays0.java
+++ BasicArrays0/BasicArrays0.java
@@ -1,1 +1,21 @@
+package programByDoing;
 
+public class BasicArrays0 {
+
+	public static void main(String[] args) {
+		int myArray[] = {-113, -113, -113, -113, -113, -113, -113, -113, -133, -133};
+		
+		System.out.println("Slot 0 contains a " + myArray[0]);
+		System.out.println("Slot 1 contains a " + myArray[1]);
+		System.out.println("Slot 2 contains a " + myArray[2]);
+		System.out.println("Slot 3 contains a " + myArray[3]);
+		System.out.println("Slot 4 contains a " + myArray[4]);
+		System.out.println("Slot 5 contains a " + myArray[5]);
+		System.out.println("Slot 6 contains a " + myArray[6]);
+		System.out.println("Slot 7 contains a " + myArray[7]);
+		System.out.println("Slot 8 contains a " + myArray[8]);
+		System.out.println("Slot 9 contains a " + myArray[9]);
+
+	}
+
+}
