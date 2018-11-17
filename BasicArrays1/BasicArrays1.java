bfb04a221ceeb9bc2e399cdb0732beaa23f74be1
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-11-17 14:22:41 -0800

    Add files via upload

--- BasicArrays1/BasicArrays1.java
+++ BasicArrays1/BasicArrays1.java
@@ -1,1 +1,15 @@
+package programByDoing;
 
+public class BasicArrays1 {
+
+	public static void main(String[] args) {
+		int myArray[] = {-113, -113, -113, -113, -113, -113, -113, -113, -133, -133};
+		
+		for (int i = 0; i < myArray.length; i++)
+		{
+			System.out.println("Slot " + i + " contains a " + myArray[i]);
+		}
+
+	}
+
+}
