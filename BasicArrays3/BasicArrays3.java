e3a2e7d61551d0cb762020babd9176a411bd9815
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-11-17 14:50:10 -0800

    Add files via upload

--- BasicArrays3/BasicArrays3.java
+++ BasicArrays3/BasicArrays3.java
@@ -1,1 +1,19 @@
+package programByDoing;
+import java.util.Random;
 
+public class BasicArrays3 {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		int myArray[] = new int[1000];
+
+		
+		for (int i = 0; i < myArray.length; i++)
+		{
+			myArray[i] = 10 + r.nextInt(99);
+			System.out.print(myArray[i] + "  ");
+		}
+
+	}
+
+}
