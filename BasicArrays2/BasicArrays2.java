540058e33afe84e7bf16aa7a155f8041676491b7
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-11-17 14:40:31 -0800

    Add files via upload

--- BasicArrays2/BasicArrays2.java
+++ BasicArrays2/BasicArrays2.java
@@ -1,1 +1,19 @@
+package programByDoing;
+import java.util.Random;
 
+public class BasicArrays2 {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		int myArray[] = new int[10];
+		
+		for (int i = 0; i < myArray.length; i++)
+		{
+			myArray[i] = 1 + r.nextInt(100);
+			System.out.println("Slot " + i + " contains a " + myArray[i]);
+		}
+		
+		System.out.println("\nThe first number is now stored even after the loop. It is " + myArray[0]);
+
+	}
+}
