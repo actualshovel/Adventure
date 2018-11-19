414d340cdf5dc2034aaf6745cf23486c05a89583
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-11-18 20:40:12 -0800

    Add files via upload

--- HowManyTimes/HowManyTimes.java
+++ HowManyTimes/HowManyTimes.java
@@ -1,1 +1,31 @@
+package programByDoing;
+import java.util.Random;
+import java.util.Scanner;
 
+public class HowManyTimes {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		Scanner kb = new Scanner(System.in);
+		int myArray[] = new int[10];
+		
+		System.out.print("Array: ");
+		for (int i = 0; i < myArray.length; i++)
+		{
+			myArray[i] = 1 + r.nextInt(50);
+			System.out.print(myArray[i] + " ");
+		}
+		System.out.print("\nNumber to find: ");
+		int find = kb.nextInt();
+		
+		int counter = 0;
+		for (int i = 0; i < myArray.length; i++)
+		{
+			if (find == myArray[i])
+				counter++;
+		}
+		
+		System.out.println(find + " was found " + counter + " times.");
+
+	}
+}
