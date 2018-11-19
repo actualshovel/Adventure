5588fa1e93d40e7ae05c20c68d4e4189a20d7f63
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-11-18 21:01:41 -0800

    Add files via upload
    Find the largest value in an array

--- LargestValueArray/LargestValueArray.java
+++ LargestValueArray/LargestValueArray.java
@@ -1,1 +1,33 @@
+package programByDoing;
+import java.util.Random;
 
+public class LargestValueArray {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		//create an array to hold 10 values
+		int myArray[] = new int[10];
+		
+		System.out.print("Array: ");
+		//put a random number in each slot
+		for (int i = 0; i < myArray.length; i++)
+		{
+			myArray[i] = 1 + r.nextInt(100);
+			System.out.print(myArray[i] + " ");
+		}
+		
+		
+		//loop through each slot in the array
+		int max = 0;
+		for (int i = 0; i < myArray.length; i++)
+		{
+			//if the current number is greater than "max"
+			if (myArray[i] > max)
+				//then current number is now the max
+				max = myArray[i];
+		}
+		//display largest number in array
+		System.out.println("\n" + max + " is the largest number.");
+
+	}
+}
