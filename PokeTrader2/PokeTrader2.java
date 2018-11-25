ed86579a0d4314eb43da098be182dac0533170f7
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-11-24 21:46:18 -0800

    Add files via upload

--- PokeTrader2/PokeTrader2.java
+++ PokeTrader2/PokeTrader2.java
@@ -1,1 +1,38 @@
+package programByDoing;
+import java.util.Scanner;
 
+public class PokeTrader2
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		String[] pokeParty = { "PIKACHU", "CHARMELEON", "GEODUDE", "GYARADOS", "BUTTERFREE", "MANKEY" };
+
+		int a;
+		int b;
+		String swap;
+
+		do
+		{
+			System.out.println("EXCHANGE POKEMON\n");
+			for ( int i=0; i<pokeParty.length; i++ )
+				System.out.println("    " + i + ". " + pokeParty[i]);
+
+			System.out.print("\nChoose a Pokemon (or -1 to quit). => ");
+			a = keyboard.nextInt();
+
+			if ( a >= 0 )
+			{
+				System.out.print("Choose a Pokemon to exchange with " + pokeParty[a] + ". => ");
+				b = keyboard.nextInt();
+
+				System.out.println("\nExchanging " + pokeParty[a] + " with " + pokeParty[b] + ".\n");
+
+				swap = pokeParty[a];
+				pokeParty[a] = pokeParty[b];
+				pokeParty[b] = swap;
+			}
+		} while ( a >= 0 );
+	}
+}
