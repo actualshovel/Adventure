8fa30e7f6b0b92d3023dc7898454d6e093bcce71
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-12-23 11:33:04 -0800

    Add files via upload

--- SortingValues/SortingValues.java
+++ SortingValues/SortingValues.java
@@ -1,1 +1,35 @@
+public class SortingValues
+{
+	public static void main( String[] args )
+	{
+		int[] arr = { 45, 87, 39, 32, 93, 86, 12, 44, 75, 50 };
 
+		// Display the original (unsorted values)
+		System.out.print("before: ");
+		for ( int i=0; i<arr.length; i++ )
+			System.out.print( arr[i] + " " );
+		System.out.println();
+
+		// Swap the values around to put them ascending order.
+		for ( int i=0; i<arr.length; i++)
+		{
+			for (int a=0 ; a<arr.length ;a++ )
+			{
+				if (arr[a] > arr[i])
+				{
+					// swap the values in two slots
+					int x = arr[a];
+					arr[a] = arr[i];
+					arr[i] = x;
+				}
+			}
+		}
+		
+
+		// Display the values again, now (hopefully) sorted.
+		System.out.print("after : ");
+		for ( int i=0; i<arr.length; i++ )
+			System.out.print( arr[i] + " " );
+		System.out.println();
+	}
+}
