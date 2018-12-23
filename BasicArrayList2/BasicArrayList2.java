37864e988b88a74a0ee7c4d60c988fdc1192c606
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-12-23 15:39:24 -0800

    Add files via upload

--- BasicArrayList2/BasicArrayList2.java
+++ BasicArrayList2/BasicArrayList2.java
@@ -1,1 +1,16 @@
+import java.util.ArrayList;
+import java.util.Random;
 
+public class BasicArrayList2 {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		for (int i=0; i<10; i++)
+			list.add(1+ r.nextInt(100));
+		
+		System.out.println("Random List: " + list);
+		System.out.println("ArrayList size: " + list.size());
+	}
+}
