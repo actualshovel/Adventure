3ffa69b0359c1664143565bd3777053b90881261
actualshovel <40729236+actualshovel@users.noreply.github.com>
2018-12-23 21:52:11 -0800

    Add files via upload

--- SortingArrayList/SortingArrayList.java
+++ SortingArrayList/SortingArrayList.java
@@ -1,1 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+public class SortingArrayList {
 
+	public static void main(String[] args) {
+		Random r = new Random();
+		ArrayList<Integer> myList = new ArrayList<Integer>(10);
+		
+		for (int i=0; i<10;i++)
+			myList.add(10 + r.nextInt(99) );
+		System.out.println("ArrayList before: " + myList);
+		
+		selectionSort(myList);
+		System.out.println("ArrayList after: " + myList);
+	}
+	
+	//sort in descending order
+	static void selectionSort(ArrayList<Integer> myList )
+	{
+		Collections.sort(myList, Collections.reverseOrder() );
+	}
+}
